310eba05ae8e911ca9460a064da1db906be7c7f3
rafin007 <rafin.ryan.07@outlook.com>
2020-01-21 22:13:02 +0600

    initial commit

---
+++ src/Components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Layout = props => {
+    return (
+        <main>
+            <div>
+                <h1 className="header" >A text header</h1>
+            </div>
+        </main>
+    );
+}
+
+export default Layout;
